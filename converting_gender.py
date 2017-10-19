e7d349977513a98247de6dbfe02555d4024a2615
RemiGui <32636831+RemiGui@users.noreply.github.com>
2017-10-19 11:26:12 +0200

    Update converting_gender.py

--- converting_gender.py
+++ converting_gender.py
@@ -4,8 +4,8 @@ def gender_conversion(matrix, column):
     :param column: Int
     :return: Converted gender : Male = 1, Female = 0
     """
-    for var_loop in range(len(matrix)):
-        if matrix[var_loop, column] == 'male':
-            matrix[var_loop, column] = 1
+    for line in range(len(matrix)):
+        if matrix[line, column] == 'male':
+            matrix[line, column] = 1
         else:
-            matrix[var_loop, column] = 0
+            matrix[line, column] = 0
