3360f1995c158851d344266ff3de59c51a0cd2e0
RemiGui <32636831+RemiGui@users.noreply.github.com>
2017-10-19 11:24:19 +0200

    Update age_prediction.py

--- age_prediction.py
+++ age_prediction.py
@@ -2,6 +2,7 @@ from linear_regression import *
 import pandas as pd
 from decision_tree import *
 
+
 def age_prediction(matrix, column):
     """
     :param matrix: Matrix
@@ -11,12 +12,12 @@ def age_prediction(matrix, column):
     train_age = []
     test_age = []
     index = []
-    for i in range(len(matrix)):
-        if pd.isnull(matrix[i][column]):
-            test_age.append(matrix[i])
-            index.append(i)
+    for line in range(len(matrix)):
+        if pd.isnull(matrix[line][column]):
+            test_age.append(matrix[line])
+            index.append(line)
         else:
-            train_age.append(matrix[i])
+            train_age.append(matrix[line])
 
     test_age = np.array(test_age)
     train_age = np.array(train_age)
@@ -30,12 +31,12 @@ def age_prediction(matrix, column):
 
     predicted_age = linear_regression(X_age, y_age, X_test_age)
 
-    for i in range(len(predicted_age)):
-        if predicted_age[i] < 0:
-            predicted_age[i] = 1
+    for line in range(len(predicted_age)):
+        if predicted_age[line] < 0:
+            predicted_age[line] = 1
 
     var = 0
-    for i in range(len(matrix)):
-        if pd.isnull(matrix[i][column]):
-            matrix[i][column] = predicted_age[var]
+    for line in range(len(matrix)):
+        if pd.isnull(matrix[line][column]):
+            matrix[line][column] = predicted_age[var]
             var += 1
